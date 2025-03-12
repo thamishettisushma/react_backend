401ae8f7b3913ee3ba124c5b306232434f853129
thamishettisushma <sushmathamishetti@gmail.com>
2025-03-12 13:13:07 +0530

    fixed cors origin wild card

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app=express()
 // app.use(cors());
 
 const corsOptions = {
-    origin: "*",
+    origin: ["http://localhost:5173", "https://react-frontend-one-henna.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
